6445a6e956b27325f174aa4f7e009e1b9bf7e30e
victormwachi <mudakizevictor@gmail.com>
2023-09-10 23:41:35 +0300

    added url check

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const server = http.createServer((req,res) =>{
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
 	if (req.url) {
-		const data ={
-  "slack_name": "victor mwachi",
-  "current_day": weekDays[new Date().getDay()],
-  "utc_time": new Date().toISOString(),
-  "track": "backend",
-  "github_file_url": "https://github.com/VictorMwachi/zuri-backend-task-one/blob/master/server.js",
-  "github_repo_url": "https://github.com/VictorMwachi/zuri-backend-task-one/",
-  "status_code": 200
+		const data = {
+			"slack_name": "victor mwachi",
+			"current_day": weekDays[new Date().getDay()],
+			"utc_time": new Date().toISOString(),
+			"track": "backend",
+			"github_file_url": "https://github.com/VictorMwachi/zuri-backend-task-one/blob/master/server.js",
+			"github_repo_url": "https://github.com/VictorMwachi/zuri-backend-task-one/",
+			"status_code": 200
 		}
-}
-	res.end(JSON.stringify(data));
+		res.end(JSON.stringify(data));
+	}
 })
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
